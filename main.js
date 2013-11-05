fa5b23e607d5bf9980a6e06ab7fdf8ad4399d5ba
CriticalNix <CriticalNix@users.noreply.github.com>
2013-11-05 09:20:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@
 // Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
 //
 // Everyone is permitted to copy and distribute verbatim or modified
-// copies of this license document, and changing it is allowed as long
+// copies of this software, and changing it is allowed as long
 // as the name is changed.
 //
 //            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
