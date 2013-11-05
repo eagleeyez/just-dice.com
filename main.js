e2b9eddf35e7c8191818f42ef129496af263d4c0
CriticalNix <CriticalNix@users.noreply.github.com>
2013-11-05 09:16:42 +0000

    Finishing up. Added extra comments. included licence. That is me done.

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
+//-------------------------------------------------------------------
+//Hi guys and girls, My name is Nix and thank you for taking the time 
+//to try this script out. I have had a lot of fun learning javascript 
+//and I always have fun playing on just-dice.com. I feel now that     
+//this script is finished and I have achieved what I originally set  
+//out to do. Feel free to use this in any way you wish, as I love    
+//open source.                                                       
+//                                                                   
+//I would like to thank darby999 and TSavo. I have learnt a  lot from
+//both of these great developers and they are both instrumental in   
+//The creation of this bot.                                          
+//                                                                   
+//Peace                                                              
+//Nix                                                                
+//                                                                   
+//-------------------------------------------------------------------
+//            Added a simple licence To ensure freedoms.
+//-------------------------------------------------------------------
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+//-------------------------------------------------------------------
+
 var timer;
 var bal;
 var bet;
-var current_steps = 1;
-var start_bet = 0;
+var current_steps = 1; //Used to find our current step
+var start_bet = 0; //value used to hold starting bet
 var $multiplier;
 var $steps;
 var $run;
-var running = false;
-var arr_ignore = new Array();
-var timer_num = 1400; //Timer delay between bets.
-var current_bet_num = 0;
-var lastBal = 0;
-var yin_yang = 0;
-var yin_yang2 = 0;
-var check_step = 0;
-var bet_total = 0;
+var running = false; //If set to true the script will start when the page loads
+var arr_ignore = new Array(); //create an array to include ignored users
+var timer_num = 1400; //Timer delay between bets. 1000 = 1 second.
+var current_bet_num = 0; //Steps counter used to find reset loss
+var lastBal = 0; //balance that is grabbed in checkstep
+var yin_yang = 0; //counting wins
+var yin_yang2 = 0; //percentage of rolls that are wins
+var check_step = 0; //Simple switch to make sure we grab the balance once
+var bet_total = 0; //Total bets =)
 
 
 // Extra buttons found on pastebin http://pastebin.com/n8X8uRAT Originally from a user called "v" and edited by another unknown user.
@@ -48,14 +81,6 @@ var profitPerMS = 0;
 function martingale() 
 {
         setTimeout(updateUSD, 1);
-        /* Stats */
-        this.stats = {
-                won: 0,
-                lost: 0,
-                maxStreak: 0,
-                currentProfit: 0,
-                wagered: 0
-        }
                 
   if (bal.data('oldVal') != bal.val() && running) {
     clearInterval(timer);
@@ -76,7 +101,7 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
             var profit = parseFloat($("#pct_balance").val()) - lastBal;
             var new_val = ($('#pct_balance').val() / 100) * ($percentage).val();
 
-            yin_yang2 = ((yin_yang/bet_total) * 100); //try % instead
+            yin_yang2 = ((yin_yang/bet_total) * 100); //win % = wins/total bets * 100 // This gives us our percentage win
 
             //get rid of scientific notation
             if (String(new_val).indexOf('e') !== -1) {
@@ -96,10 +121,10 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
 			bet_total++;
             //current_bet_num = 1;
             $("#a_hi").trigger('click');
-            $("#win_lose").val((yin_yang2).toFixed(2));
-			$("#pro_fits").val((profit).toFixed(8));
-			$("#Bet_amt").val(bet_total);
-        }    
+            $("#win_lose").val((yin_yang2).toFixed(2)); //Update win %
+			$("#pro_fits").val((profit).toFixed(8)); //Update Profit
+			$("#Bet_amt").val(bet_total); //Update bet counter
+        }    //end of reset loss step
 
    else if (curr_bal > bal.data('oldVal')) //This is win step
     {
@@ -110,7 +135,7 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
             var profit = parseFloat($("#pct_balance").val()) - lastBal;
             var new_val = $("#pct_bet").val(); // Why I had left a multiplyer here.. Madness Fixed now.
 
-            yin_yang2 = ((yin_yang/bet_total) * 100); //try % instead
+            yin_yang2 = ((yin_yang/bet_total) * 100); //win % = wins/total bets * 100 // This gives us our percentage win
 
             //get rid of scientific notation
             if (String(new_val).indexOf('e') !== -1) {
@@ -130,10 +155,10 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
 			yin_yang++;
 			bet_total++;
             $("#a_hi").trigger('click');
-            $("#win_lose").val((yin_yang2).toFixed(2));
-            $("#pro_fits").val((profit).toFixed(8));
-			$("#Bet_amt").val(bet_total);
-        }
+            $("#win_lose").val((yin_yang2).toFixed(2)); //Update win %
+			$("#pro_fits").val((profit).toFixed(8)); //Update Profit
+			$("#Bet_amt").val(bet_total); //Update bet counter
+        } //end of win step
     
         
     else if ($.isNumeric($multiplier.val()) && // This is loss step
@@ -144,7 +169,7 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
             var profit = parseFloat($("#pct_balance").val()) - lastBal;
             var new_val = $("#pct_bet").val() * $multiplier.val();
 
-            yin_yang2 = ((yin_yang/bet_total) * 100); //try % instead
+            yin_yang2 = ((yin_yang/bet_total) * 100); //win % = wins/total bets * 100 // This gives us our percentage win
 
             //get rid of scientific notation
             if (String(new_val).indexOf('e') !== -1) {
@@ -156,9 +181,9 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
 
             
             $("#pct_bet").val(new_val);
-            $("#win_lose").val((yin_yang2).toFixed(2));
-			$("#pro_fits").val((profit).toFixed(8));
-			$("#Bet_amt").val(bet_total);
+            $("#win_lose").val((yin_yang2).toFixed(2)); //Update win %
+			$("#pro_fits").val((profit).toFixed(8)); //Update Profit
+			$("#Bet_amt").val(bet_total); //Update bet counter
 
             //Increase the steps
             current_steps++;
@@ -166,19 +191,19 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
 			//yin_yang--;
 			bet_total++;
             $("#a_hi").trigger('click');
-    }
+    } //end of loss step
 
     //otherwise we go back to the start
     else {  //This is bust step
 
-      yin_yang2 = ((yin_yang/bet_total) * 100); //try % instead
+      yin_yang2 = ((yin_yang/bet_total) * 100); //win % = wins/total bets * 100 // This gives us our percentage win
       current_steps = 1;
       current_bet_num = 0;
       $("#pct_bet").val(start_bet);
       $("#win_lose").val((yin_yang2).toFixed(2));
 	  $("#pro_fits").val((profit).toFixed(8));
       running = false;
-    }
+    } //end of bust step
 
     // Updated stored value
     bal.data('oldVal', bal.val());
@@ -192,7 +217,7 @@ if (current_bet_num == $delay.val() && curr_bal < bal.data('oldVal')) // this is
 
 // A little bit of a help file.
 function tabber() {
-        var markup = '<div class="bot-stats"><div>Hi Guys and girls thankyou for taking the time to try or use this automated betting system. My name is (98066)Nix You can usually find me right here in the chat on JD. If you need to ask anything feel free to, I will help all I can.<div></div> It has been a lot of fun learning some javascript and it is even more fun trying out new ways (I know trying.) to beat the house at JD.<div></div> If you win loads or just like this bot consider donating a coffee and a pizza =) 1NixsyLiMFX3wwLqdtAVsNLsWwqDpbmuhP  </div><h3>Multiplyer</h3><div>This is a value used to increase bet on loss eg. 2x multiplier will double your bet on a loss</div><div class="clear"></div><h3>Max losses</h3><div>This is the amount of consecutive losses you want the bot to be able to handle, The bot will stop upon reaching a loss streak of this length unless Reset loss is a smaller number</div><h3>Reset loss</h3><div>This is a relatively new feature to martingale. Upon a loss streak reaching this number. the bet value will change to value given in Reset %</div><h3>Reset %</h3><div>This value is called when Reset loss is reached. This value is a percentage of your total bank. Use extreme caution when setting high numbers here  </div><h3>Credits</h3><div>I would like to thank Darby999 for his original script. he was laid up in bed with a broken hip in spring this year and the origins of this script was born. I would also like to thank TSavo as without him The currency converter would not exist.</div><h3></h3><div></div><h3>A word of warning</h3><div>Any sort of automated betting system will ultimately contain bugs. Do not ever have more in your balance than you are willing to lose and always use google two factor authentication.</div><div class="clear"></div><div class="bot-graph">Check here for updates and new changes https://github.com/CriticalNix/just-dice.com</div><div class="bot-foot">';
+        var markup = '<div class="bot-stats"><div>Hi Guys and girls thankyou for taking the time to try or use this automated betting system. My name is (98066)Nix You can usually find me right here in the chat on JD. If you need to ask anything feel free to, I will help all I can.<div></div> It has been a lot of fun learning some javascript and it is even more fun trying out new ways (I know trying.) to beat the house at JD.<div></div> If you win loads or just like this bot consider donating a coffee and a pizza =) 1NixsyLiMFX3wwLqdtAVsNLsWwqDpbmuhP  </div><h3>Multiplyer</h3><div>This is a value used to increase bet on loss eg. 2x multiplier will double your bet on a loss</div><div class="clear"></div><h3>Max losses</h3><div>This is the amount of consecutive losses you want the bot to be able to handle, The bot will stop upon reaching a loss streak of this length unless Reset loss is a smaller number</div><h3>Reset loss</h3><div>This is a relatively new feature to martingale. Upon a loss streak reaching this number. the bet value will change to value given in Reset %</div><h3>Reset %</h3><div>This value is called when Reset loss is reached. This value is a percentage of your total bank. Use extreme caution when setting high numbers here.</div><h3>Profit display</h3><div>This will show your profit won. If you refresh the page this value will reset.</div><h3>Win % display</h3><div>This will show wins as a percentage of rolls. You can expect this number to be very close to chance to win</div><h3>Credits</h3><div>I would like to thank Darby999 for his original script. he was laid up in bed with a broken hip in spring this year and the origins of this script was born. I would also like to thank TSavo as without him The currency converter would not exist.</div><h3></h3><div></div><h3>A word of warning</h3><div>Any sort of automated betting system will ultimately contain bugs. Do not ever have more in your balance than you are willing to lose and always use google two factor authentication.</div><div class="clear"></div><div class="bot-graph">Check here for updates and new changes https://github.com/CriticalNix/just-dice.com</div><div class="bot-foot">';
                 $panelWrapper = $('<div>').attr('id','Nixsy9').css({display: 'none'}).insertAfter('#faq'),
                 $panel = $('<div>').addClass('panel').append(markup).appendTo($panelWrapper),
                                 
