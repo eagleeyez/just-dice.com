ee2423dd0162e937442e646d2956fd65e9ad3d70
CriticalNix <neo_twoc@live.co.uk>
2013-11-15 17:28:32 +0000

    var xmlDoc = new ActiveXObject throws errors, Dang thanks google!

--- main.js
+++ main.js
@@ -51,9 +51,6 @@ var bet_total = 0; //Total bets
 var new_val = 0.00000001;
 var snd = new Audio('http://www.soundjay.com/button/sounds/beep-7.mp3');
 var coin_drop = new Audio('http://www.soundjay.com/misc/sounds/coin-drop-1.mp3');
-var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-var FILENAME = 'C:\\Users\\Nix\\Desktop\\save-test\\BetsXML2.xml';
 
 // Extra buttons found on pastebin http://pastebin.com/n8X8uRAT Originally from a user called "v" and edited by another unknown user.
 
@@ -604,60 +601,7 @@ $(document).ready(function () {
 	});
 
 });
-//---------------  xml block   ---------------------
-    function SaveXML(BetData)
-    {  
-        var file = fso.CreateTextFile(FILENAME, true);
-        file.WriteLine('<?xml version="1.0" encoding="utf-8"?>\n');
-        file.WriteLine('<BetsInfo>\n');
- 
-        for (countr = 0; countr < BetData.length; countr++) {
-            file.Write('    <Bets ');
-            file.Write('Bet="' + BetData[countr][0] + '" ');
-            file.WriteLine('></Bets>\n');
-        } // end for countr
- 
-        file.WriteLine('</BetsInfo>\n');
-        file.Close();
- 
-    } // end SaveXML function --------------------
- 
-    function LoadXML(xmlFile)
-    {
-        xmlDoc.load(xmlFile);
-        return xmlDoc.documentElement;
-    } //end function LoadXML()
- 
-    function initialize_array()
-    {
-        var Bets = new Array();
-        var noFile = true;
-        var xmlObj;
-        if (fso.FileExists(FILENAME))
-        {
-            xmlObj = LoadXML(FILENAME);
-            noFile = false;
-            } // if
-        else
-        {
-            xmlObj = LoadXML("BetsXML.xml");
-            //alert("local" + xmlObj);
-            } // end if
- 
-        var BetCount = 0;
-        while (BetCount < xmlObj.childNodes.length)
-        {
-            var tmpBets = new Array(xmlObj.childNodes(BetCount).getAttribute("Bet")
-            Bets.push(tmpBets);
-            BetCount++;
-             }   //end while
-        if (noFile == false)
-            fso.DeleteFile(FILENAME);
-        SaveXML(Bets);
-    }   // end function initialize_array()
-    
-//---------------  end xml block   ---------------------    
-    
+
 (function ($) {
 
 	  $.extend({
