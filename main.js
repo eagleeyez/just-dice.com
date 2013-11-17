f3e4a91c30338f59ad0b0c76bee80846233c1448
CriticalNix <neo_twoc@live.co.uk>
2013-11-17 22:10:32 +0000

    Tidied gui up a little.

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ var current_win_per = 0;
 var rndhilo = 1;
 
 function appendVersion() { 
-	var footer = "<div style='position:fixed;bottom:0px;background-color:white;'>Bot version 1.0.1</div>"
+	var footer = "<div style='position:fixed;bottom:0px;background-color:white;'>Bot version 1.0.2</div>"
 		$("body").append(footer);
 }
 
@@ -327,13 +327,13 @@ function create_ui() {
 
 	  var $container = $('<div id="chipper" class="container"/>');
 	var $container2 = $('<div class="container"/>');
-	  var $button_group = $('<div style="width:99%;background-color:#878787;border:2px solid; border-color: #6E6E6E;" class="button_group"/>');
+	  var $button_group = $('<div style="width:99%;background-color:#787878 ;border:2px solid; border-color: #505050;" class="button_group"/>');
 	  $container.append($button_group);
 
-	  var $martingale_button = $('<button class="button_label chance_toggle" style="margin-top:38px;margin-right:3px;height:65px;;width:70px;color:transparent;background-color:transparent;border:none;"><img src="http://i.imgur.com/e3LQ30h.png"></button>');
+	  var $martingale_button = $('<button class="button_label chance_toggle" style="margin-top:44px;margin-right:3px;height:65px;;width:70px;color:transparent;background-color:transparent;border:none;"><img src="http://i.imgur.com/e3LQ30h.png"></button>');
 
-	  var $run_div = $('<div class="button_inner_group"/>');
-	  $run = $('<button id="c_run" style="margin-top:50px;margin-left:8px;">Go</button>');
+	  var $run_div = $('<div style="background-color:#787878;margin-top:50px;border:2px solid; border-color: #505050;" class="button_inner_group"/>');
+	  $run = $('<button id="c_run" style="margin-bottom:5px;margin-top:5px;margin-left:5px;">Go</button>');
 
 	  $run.click(function () {
 		        running = true;
@@ -345,7 +345,7 @@ function create_ui() {
 
 	StartBalance = parseFloat($("#pct_balance").val()); // Try to add profit readout
 	    
-	  $Stop = $('<button id="c_stop" style="margin-top:50px;margin-left:8px;">Stop</button>');
+	  $Stop = $('<button id="c_stop" style="margin-bottom:5px;margin-top:5px;margin-right:5px;margin-left:5px;">Stop</button>');
 	  $Stop.click(function () {
 		  running = false;
 		  $("#pct_bet").val(start_bet);
